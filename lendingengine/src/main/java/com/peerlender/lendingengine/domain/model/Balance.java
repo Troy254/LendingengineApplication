cf3eab357cb82d84ca9883f6818f6adceb12c59a
Troy.Andati <andatisiro@gmail.com>
2024-02-19 13:49:05 +0300

    BalanceClass

--- lendingengine/src/main/java/com/peerlender/lendingengine/domain/model/Balance.java
+++ lendingengine/src/main/java/com/peerlender/lendingengine/domain/model/Balance.java
@@ -9,4 +9,11 @@ public class Balance {
 
   private Map<Currency,Money> moneyMap = new HashMap<>();
 
+  public void topUp(Money money) {
+    if(moneyMap.get(money.getCurrency()) == null) {
+      moneyMap.put(money.getCurrency(),money);
+    } else {
+      moneyMap.put(money.getCurrency(),moneyMap.get(money.getCurrency()).add(money));
+    }
+  }
 }
