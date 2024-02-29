40601135f57204473cbf007f9f20a526555b4b60
Troy.Andati <andatisiro@gmail.com>
2024-02-29 07:30:58 +0300

    exchange money

--- lendingengine/src/main/java/com/peerlender/lendingengine/domain/service/LoanService.java
+++ lendingengine/src/main/java/com/peerlender/lendingengine/domain/service/LoanService.java
@@ -2,11 +2,14 @@ package com.peerlender.lendingengine.domain.service;
 
 import com.peerlender.lendingengine.domain.exception.UserNotFoundException;
 import com.peerlender.lendingengine.domain.model.AppUsers;
+import com.peerlender.lendingengine.domain.model.Currency;
 import com.peerlender.lendingengine.domain.model.Loan;
 import com.peerlender.lendingengine.domain.model.LoanApplication;
+import com.peerlender.lendingengine.domain.model.Money;
 import com.peerlender.lendingengine.domain.repository.LoanApplicationRepository;
 import com.peerlender.lendingengine.domain.repository.LoanRepository;
 import com.peerlender.lendingengine.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,12 +28,15 @@ public class LoanService {
     this.userRepository = userRepository;
     this.loanRepository = loanRepository;
   }
-
+  @Transactional
   public void acceptLoan(String applicationId, String lenderUsername) {
     System.out.println("passed through service");
     AppUsers lender = findAppUsers(lenderUsername);
     LoanApplication loanApplication = findLoanApplication(applicationId);
     AppUsers borrower = loanApplication.getBorrower();
+    Money money = new Money(loanApplication.getAmount(), Currency.USD);
+    lender.withdraw(money);
+    borrower.topUp(money);
     loanRepository.save(new Loan(lender, loanApplication));
   }
 
