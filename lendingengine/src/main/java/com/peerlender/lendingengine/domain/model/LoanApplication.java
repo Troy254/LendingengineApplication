cbc458fbc70bf0268005f01bbb64806158504be0
Troy.Andati <andatisiro@gmail.com>
2024-03-14 15:11:15 +0300

    LoanApp edit

--- lendingengine/src/main/java/com/peerlender/lendingengine/domain/model/LoanApplication.java
+++ lendingengine/src/main/java/com/peerlender/lendingengine/domain/model/LoanApplication.java
@@ -7,9 +7,7 @@ import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 public class LoanApplication {
 
@@ -21,4 +19,58 @@ public class LoanApplication {
   private int repaymentTermInDays;
   private double interestRate;
 
+  public LoanApplication(int amount, AppUsers borrower, int repaymentTermInDays,
+      double interestRate) {
+    this.amount = amount;
+    this.borrower = borrower;
+    this.repaymentTermInDays = repaymentTermInDays;
+    this.interestRate = interestRate;
+  }
+
+  public LoanApplication() {
+
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public AppUsers getBorrower() {
+    return borrower;
+  }
+
+  public int getRepaymentTermInDays() {
+    return repaymentTermInDays;
+  }
+
+  public double getInterestRate() {
+    return interestRate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    LoanApplication that = (LoanApplication) o;
+    return amount == that.amount && repaymentTermInDays == that.repaymentTermInDays
+        && Double.compare(interestRate, that.interestRate) == 0 && Objects.equals(
+        borrower, that.borrower);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(amount, borrower, repaymentTermInDays, interestRate);
+  }
+
+  @Override
+  public String toString() {
+    return "LoanApplication{" +
+        "amount=" + amount +
+        ", borrower=" + borrower +
+        ", repaymentTermInDays=" + repaymentTermInDays +
+        ", interestRate=" + interestRate +
+        '}';
+  }
 }
